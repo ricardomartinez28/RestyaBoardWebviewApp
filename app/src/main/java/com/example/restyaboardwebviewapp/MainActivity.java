5998cf7638d7e71281e0606172b3fd8644e4964a
ricardomartinez <ricardomartinezi28985@gmail.com>
2019-04-15 11:00:24 +0100

    Make Some test

--- app/src/main/java/com/example/restyaboardwebviewapp/MainActivity.java
+++ app/src/main/java/com/example/restyaboardwebviewapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.restyaboardwebviewapp;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.CookieSyncManager;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.net.CookieManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +24,38 @@ public class MainActivity extends AppCompatActivity {
 
         webView=findViewById(R.id.wv_main);
 
+
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://restya.com");
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+
+        webView.setWebViewClient(new WebViewClient()
+    {
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler
+       handler, SslError error)
+        {
+            handler.proceed();
+        }
+    });
+
+        webView.loadUrl("https://restya.pw/");
+
+        webView.setWebChromeClient(new WebChromeClient()
+        {
+
+        });
+
+
+        webView.loadUrl("https://restya.pw/");
+
+
+
     }
 }
